a66b87bb4d297c5ee7fdef7cafffd402f06d9f61
Oghenekparobo <classicsteph11@gmail.com>
2022-10-07 17:16:25 +0100

    database for users setup

---
+++ admin/comments.php
@@ -0,0 +1,42 @@
+<?php include("includes/header.php"); ?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    <?php include("includes/top_nav.php") ?>
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <?php include("includes/side_nav.php") ?>
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div id="page-wrapper">
+
+    <!-- Page Heading -->
+
+    <div class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Comments
+                    <small>Subheading</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Blank Page
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
---
+++ admin/css/includes/admin_content.php
@@ -0,0 +1,24 @@
+ <!-- Page Heading -->
+
+ <div class="container-fluid">
+
+     <div class="row">
+
+         <div class="col-lg-12">
+             <h1 class="page-header">
+                 Dashboard
+                 <small>Subheading</small>
+             </h1>
+             <ol class="breadcrumb">
+                 <li>
+                     <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
+                 </li>
+                 <li class="active">
+                     <i class="fa fa-file"></i> Blank Page
+                 </li>
+             </ol>
+         </div>
+     </div>
+     <!-- /.row -->
+
+ </div>
